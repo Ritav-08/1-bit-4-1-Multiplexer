edefea2a38ed36a8f4f08ea3c8117d8306f1c34f
Ritav-08 <ritav08092004@gmail.com>
2026-04-09 18:04:04 +0530

    Update mux4t1.v

--- mux4t1.v
+++ mux4t1.v
@@ -1,26 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Author: Ritav
-//
-// Create Date: 08.04.2026 13:42:20
-// Design Name: 1-bit 4:1 Multiplexer
-// Module Name: mux4t1
-// Target Devices: Simulation Only
-// Tool Versions: Vivado 2025.2
-// Description: 
-//     4:1 Mux (mux4t1) using 2:1 Mux (mux2t1), structural representation, enable port.
-// Dependencies: 
-//     mux4t1.v, mux2t1.v
-// Revision:
-//     Revision 0.01 - File Created
-//     Revision 0.02 - fixed unknown behaviour encountered during simulation (at enable=0, any input undefined)
 
-// Additional Comments:
-// I[0]-I[2] as mux-1 inputs, I[1]-I[3] as mux-2 inputs. This order is chosen so that: sel=00 -> I[0], sel=01 -> I[1], sel=10 -> I[2], sel=11 -> I[3].
-// When Enable is 0, Output is: X when any input is undefined, 0 when all inputs are defined
-//////////////////////////////////////////////////////////////////////////////////
+//Checkout Readme for more details
 
-//-----------------------------------------------
 //1-bit 4:1 MUX using 2:1 MUX- Structural representation
 module mux4t1(in, en, sel, dout);
 input en; //enable
